b45422cc792f4658512f87f2d804b8739d669734
Tahir Mia <t.sidikmia@gmail.com>
2019-03-10 19:32:13 +0000

    Created base of game.
    Created base of game but still some things need to be changed and fixed.

---
+++ base.js
@@ -0,0 +1,65 @@
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
+
+function setup() {
+  createCanvas(500, 500);
+	w = floor(width / rez);
+	h = floor(height / rez);
+	frameRate(15);
+	snake = new Snake();
+	foodObj();
+
+}
+
+
+
+function foodObj(){
+	let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+}
+
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+  	snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+  	snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+  	snake.setDir(0, -1);
+  } else if (key == ' ') {
+  	snake.grow();
+  }
+
+}
+
+
+
+
+
+function draw() {
+  scale(rez);
+  background(133);
+  if (snake.eat(food)) {
+     foodLocation();
+  }
+  snake.update();
+  snake.show();
+  
+  
+  
+  if (snake.gameLost()) {
+  	print("Game Lost");
+    background(255, 0, 0);
+    noLoop();
+  }
+  
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
